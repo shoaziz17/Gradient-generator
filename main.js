7721f14d85e2db207094cc0ec473ad43c4d67d87
shoaziz17 <shoazizrixsiboyev03@gmail.com>
2023-06-26 15:32:53 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const hexValue = document.querySelector("p");
+const values = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
+
+function getGradient() {
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    const randomNumber = Math.trunc(Math.random() * values.length);
+    color += values[randomNumber];
+  }
+  return color;
+}
+
+function setGradient() {
+  const color1 = getGradient();
+  const color2 = getGradient();
+  const randomDeg = Math.trunc(Math.random() * 360);
+  const bgColor = `linear-gradient(
+    ${randomDeg}deg,
+    ${color1},
+    ${color2}
+  )`;
+
+  body.style.background = bgColor;
+  hexValue.innerHTML = bgColor;
+}
+
+setGradient();
+
+container.addEventListener("click", setGradient);
